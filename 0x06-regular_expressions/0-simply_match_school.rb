a1ae5c98517dd664a100e54a6b62a89c344c52ce
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-11-03 13:51:22 -0700

    fixed error in match School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,11 +1,3 @@
 #!/usr/bin/env ruby
 # This regular expression must match School
-regex = /School/
-
-match = ARGV[0].match(regex)
-
-if match
-	puts match[0]
-else
-	puts ''
-end
+puts ARGV[0].scan(/School/).join
