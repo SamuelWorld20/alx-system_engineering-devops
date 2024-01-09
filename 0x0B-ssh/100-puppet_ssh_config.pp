2678260a63041cbe2c7daa40b84ce6e6ffa6b498
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2024-01-09 08:21:09 -0800

    config ssh using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/pup
+# config ssh config file to login to server without password using puppet
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
